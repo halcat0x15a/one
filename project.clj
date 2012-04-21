f0706362eb965bac0db1468157918594784526c0
Sanshiro Yoshida <halcat0x15a@gmail.com>
2012-04-21 18:46:32 +0900

    add project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject onedit "0.1-SNAPSHOT"
+  :description "online editor"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-cljsbuild "0.1.8"]]
+  :cljsbuild {
+              :builds [{
+                        :source-path "src/main/cljs"
+                        :compiler {
+                                   :output-to "src/main/resources/public/onedit.js"
+                                   :pretty-print true}}]})
