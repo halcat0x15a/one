b1a52e355239b85244cdfbc0b0e6d0372f3e21a9
Sanshiro Yoshida <halcat0x15a@gmail.com>
2012-04-24 06:59:01 +0900

    remove files

--- src/main/resources/highlight.py
+++
@@ -1,7 +0,0 @@
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters import HtmlFormatter
-
-lexer = get_lexer_by_name(lang)
-formatter = HtmlFormatter()
-result = highlight(code, lexer, formatter)
--- src/main/resources/lexers.py
+++
@@ -1,5 +0,0 @@
-from pygments.lexers import get_all_lexers
-
-import simplejson as json
-
-result = json.dumps(map(lambda lexer: {'name':lexer[0], 'alias':lexer[1][0]}, get_all_lexers()))
