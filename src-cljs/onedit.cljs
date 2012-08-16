01552323ebce0a57b5a9c364a46f74064b76df26
Sanshiro Yoshida <halcat0x15a@gmail.com>
2012-08-17 05:51:03 +0900

    create base js

--- src-cljs/onedit.cljs
+++ src-cljs/onedit.cljs
@@ -1,5 +1,34 @@
 (ns onedit
-  (:require [goog.fs.FileReader :as file-reader]))
+  (:require [clojure.string :as string]
+            [clojure.browser.dom :as dom]
+            [clojure.browser.event :as event]
+            [goog.events.EventType :as event-type]))
 
-(defn open []
-  (let [reader (FileReader.)]))
+(defrecord Editor [buffer cursor])
+
+(defn open [file]
+  (let [reader (js/FileReader.)]
+    (.readAsText reader file)))
+
+(defn save [])
+
+(defn insert [string editor]
+  (let [buffer (string/join (split-at (:cursor editor) (:buffer editor)))]
+    (assoc editor :buffer buffer)))
+
+(def functions [insert])
+
+(defn function-map []
+  functions)
+
+(defn exec [buffer minibuffer event]
+  (let [value (dom/get-value minibuffer)
+        [f & args] (string/split value #" ")]
+    (dom/log f)
+    (dom/log args)
+    (dom/set-text buffer "")))
+
+(defn main []
+  (let [buffer (dom/get-element "buffer")
+        minibuffer (dom/get-element "minibuffer")]
+    (event/listen minibuffer event-type/CHANGE (partial exec buffer minibuffer))))
